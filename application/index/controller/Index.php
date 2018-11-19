28d5852031392f40ce3b31754eb621b5421be98c
bishyan <maijie5201314@163.com>
2018-11-19 15:54:41 +0800

    Initial commit

---
+++ application/index/controller/Index.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    // 微信验证
+    public function index()
+    {
+        // 获得参数 signaturn nonce token timestamp
+        $timestamp = $_GET['timestamp'];
+        $nonce = $_GET['nonce'];
+        $token = 'GUO20160704guo';
+        $signture = $_GET['signature'];
+        $arr = array($timestamp, $token, $nonce);
+        sort($arr);
+        
+        $tmpstr = implode('', $arr);
+        $tmpstr = sha1($tmpstr);
+        
+        if ($tmpstr == $signture) {
+            return $_GET['echostr'];
+        }
+        //return  'hello weixin';
+    }
+}

3e2360696241adb42de746337464fe72bc70cbe9
bishyan <maijie5201314@163.com>
2018-11-19 22:18:44 +0800

    Update Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -32,7 +32,9 @@ class Index
     // 接收事件推送并回复
     public function responseMsg() {
         // 1. 获取到微信推送过来的post数据(xml格式)
-        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        //$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        var_dump($GLOBALS);
+        var_dump($_POST); exit;
         // 2. 处理消息类型, 并设置回复类型和内容
         /*<xml><ToUserName>< ![CDATA[toUser] ]></ToUserName>
          * <FromUserName>< ![CDATA[FromUser] ]></FromUserName>

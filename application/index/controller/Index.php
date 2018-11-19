351f343ec1a07d59afe5b9377e5769cc4e2227d1
bishyan <maijie5201314@163.com>
2018-11-19 16:47:57 +0800

    Update Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -11,14 +11,18 @@ class Index
         $nonce = $_GET['nonce'];
         $token = 'GUO20160704guo';
         $signature = $_GET['signature'];
+        $echostr = $_GET['echostr'];
+        
+        // 按字典排序
         $arr = array($timestamp, $token, $nonce);
         sort($arr);
         
+        // 拼接成字符串, 并用sha1加密, 然后与$signature进行校验
         $tmpstr = implode('', $arr);
         $tmpstr = sha1($tmpstr);
         
         if ($tmpstr == $signature) {
-            return $_GET['echostr'];
+            return $echostr;
         }
         //return  'hello weixin';
     }

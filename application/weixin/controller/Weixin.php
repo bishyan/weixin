db47c6b28b2c45b406cbc94f4ad162497b45084f
bishyan <maijie5201314@163.com>
2018-11-20 15:41:03 +0800

    Update Weixin.php

--- application/weixin/controller/Weixin.php
+++ application/weixin/controller/Weixin.php
@@ -181,20 +181,27 @@ class Weixin extends Controller  {
     }
     
     public function getWxAccessToken() {
-        // 1. 初始化curl
-        $curl = curl_init();
-        
-        // 2. 设置curl的参数
+        // 1. 请求url地址
         $appid = 'wxe6dfc606143872e8';
         $secret = '0be5d18ee3d0f3df973d06c2a9e22b22';
         $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='. $appid .'&secret='. $secret;
+        
+        // 2. 初始化curl
+        $curl = curl_init();     
+        // 3. 设置参数        
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        // 3. 
+        // 4. 调用接口 
         $res = curl_exec($curl);
+
+        if (curl_errno($curl)) {
+            var_dump(curl_error($curl));
+        }
+        
+        $res = json_decode($res, true);
         var_dump($res);
-        // 4. 关闭
-        curl_close($curl);
+        // 5. 关闭
+        curl_close($curl); 
     }
 }
 

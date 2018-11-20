7f0515ba7f4da81ab6fe556079e4d11bfef6a8a9
bishyan <maijie5201314@163.com>
2018-11-20 10:52:40 +0800

    新增自动回复功能

--- application/weixin/controller/Weixin.php
+++ application/weixin/controller/Weixin.php
@@ -59,7 +59,7 @@ class Weixin extends Controller  {
                     $fromUser = $postObj->ToUserName;
                     $time = time();
                     $msgType = 'text';
-                    $content = '欢迎关注果果爸爸的订阅号';
+                    $content = '欢迎关注果果爸爸的订阅号, \n回复1: 了解果果的年龄, \n回复2: 了解果果的身高, \n回复3: 了解果果的体重';
                     /*$template = "<xml> 
                             <ToUserName><![CDATA[%s]]></ToUserName> 
                             <FromUserName><![CDATA[%s]]></FromUserName> 
@@ -80,6 +80,19 @@ class Weixin extends Controller  {
                     //echo $info;
                 }
             } else if ($postObj->MsgType == 'text') {
+                switch($postObj->Content) {
+                    case 1:
+                        $content = '果果2岁4个月大了..';
+                        break;
+                    case 2:
+                        $content = '果果目前的身高是86cm';
+                        break;
+                    case 3:
+                        $content = '果果13kg了';
+                    default:
+                        $content = '关键字不正确!';
+                        break;
+                }
                 $toUser = $postObj->FromUserName;
                 $fromUser = $postObj->ToUserName;
                 $time = time();
@@ -91,12 +104,6 @@ class Weixin extends Controller  {
                         <MsgType><![CDATA[%s]]></MsgType>
                         <Content><![CDATA[%s]]></Content>
                         </xml>";
-                
-                if ($postObj->Content == '果果') {                    
-                    $content = '果果2岁4个月大了...';   
-                } else if (strtolower($postObj->Content) == '身高') {
-                    $content = '果果身高85CM';
-                }
                 
                  printf($template, $toUser, $fromUser, $time, $type, $content);
             }

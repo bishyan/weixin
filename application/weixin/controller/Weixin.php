38bc1badc33aae1c51ad013662ce3165a92b3d66
bishyan <maijie5201314@163.com>
2018-11-20 10:07:25 +0800

    Update Weixin.php

--- application/weixin/controller/Weixin.php
+++ application/weixin/controller/Weixin.php
@@ -23,9 +23,50 @@ class Weixin extends Controller  {
             echo $echostr;
             exit;
         } else {
-            $this->responseMsg();
+            $this->responseMessage();
         }
     }
+    
+        // 接收事件推送并回复
+    public function responseMessage() {
+        // 1. 获取到微信推送过来的post数据(xml格式)
+        //$postStr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        $postStr = file_get_contents("php://input"); 
+        //2.处理消息类型，并设置回复类型和内容
+        if (!empty($postStr)) {
+            $postObj = simplexml_load_string( $postStr );
+            // 判断数据包是否是订阅的事件推送
+            if (strtolower($postObj->MsgType) == 'event') {
+                // 如果是关注subscribe 事件
+                if (strtolower($postObj->Event) == 'subscribe') {
+                    // 回复用户消息
+                    $toUser = $postObj->FromUserName;
+                    $fromUser = $postObj->ToUserName;
+                    $time = time();
+                    $msgType = 'text';
+                    $content = '欢迎关注果果爸爸的订阅号.....'.$postObj->FromUserName . '-' . $postObj->ToUserName;
+
+                    /*<xml>  回复文本消息的模板
+                     * <ToUserName>< ![CDATA[toUser] ]></ToUserName>
+                     * <FromUserName>< ![CDATA[fromUser] ]></FromUserName> 
+                     * <CreateTime>12345678</CreateTime>
+                     * <MsgType>< ![CDATA[text] ]></MsgType> 
+                     * <Content>< ![CDATA[你好] ]></Content> 
+                     * </xml>
+                     */
+                    $template = "<xml> 
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            </xml>"; 
+
+                    $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                    echo $info;
+
+                } 
+            }
     
     public function responseMsg() {
         // 1.获取到微信推送过来的post数据(xml格式)

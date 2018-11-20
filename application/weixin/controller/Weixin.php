f172789e50391f151f1a5e2b2e542d8f2bc39e0e
bishyan <maijie5201314@163.com>
2018-11-20 08:57:25 +0800

    新增微信模块

---
+++ application/weixin/controller/Weixin.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\weixin\controller;
+
+use think\Controller;
+
+class Weixin extends Controller  {
+    public function index() {
+        // 接收参数 signature nonce token echostr
+        $timestamp = $_GET['timestamp'];
+        $nonce = $_GET['nonce'];
+        $token = 'guoguo2016';
+        $signature = $_GET['signature'];
+        $echostr = $_GET['echostr'];
+        // 将timestamp, nonce, token 三个参数按字典排序, sha1加密, 然后与signature校对
+        $arr = array($timestamp, $nonce, $token);
+        sort($arr);
+        $arrStr = sha1($arr);
+        if ($arrStr == $signature && $signature) {
+            echo $echostr;
+            exit;
+        } else {
+            $this->responseMsg();
+        }
+    }
+    
+    public function responseMsg() {
+        // 1.接收微信发送过来的参数
+        $postStr = file_get_contents('php://input');
+        // 2. 处理数据并回复
+        $postObj = simplexml_load_string($postStr);
+        if (!$postObj) {
+            // 判断是否是推送事件
+            /*<xml><ToUserName>< ![CDATA[toUser] ]></ToUserName><FromUserName>< ![CDATA[FromUser] ]></FromUserName><CreateTime>123456789</CreateTime><MsgType>< ![CDATA[event] ]></MsgType><Event>< ![CDATA[subscribe] ]></Event></xml>*/
+            
+            if (strtolower($postObj->MsgType) == 'event') {
+                // 如果是关注事件(subscride)
+                if (strtolower($postObj->Event) == 'subscride') {
+                    //拼接回复消息(纯文本)
+                    /*<xml> 
+                     * <ToUserName>< ![CDATA[toUser] ]></ToUserName> 
+                     * <FromUserName>< ![CDATA[fromUser] ]></FromUserName> 
+                     * <CreateTime>12345678</CreateTime> 
+                     * <MsgType>< ![CDATA[text] ]></MsgType> 
+                     * <Content>< ![CDATA[你好] ]></Content> 
+                     * </xml>*/
+                    $toUser = $postObj->FromUserName;
+                    $fromUser = $postObj->ToUserName;
+                    $time = time();
+                    $msgType = 'text';
+                    $content = '感谢关注果爸的公众号';
+                    $template = "<xml> 
+                            <ToUserName>< ![CDATA[%s] ]></ToUserName> 
+                            <FromUserName>< ![CDATA[%s] ]></FromUserName> 
+                            <CreateTime>%s</CreateTime> 
+                            <MsgType>< ![CDATA[%s] ]></MsgType> 
+                            <Content>< ![CDATA[%s] ]></Content> 
+                            <xml>";
+                    echo printf($template, $toUser, $fromUser, $time, $msgType, $content);
+                }
+            }
+        }
+    }
+}
+

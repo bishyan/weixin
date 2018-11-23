4e23bd6d0c8b9a51285d98645a27a93c85256461
bishyan <maijie5201314@163.com>
2018-11-23 11:16:20 +0800

    Update Index.php

--- application/weixin/controller/Index.php
+++ application/weixin/controller/Index.php
@@ -40,6 +40,8 @@ class Index extends Controller  {
                 } else if (strtolower($postObj->Event) == 'click') {
                     if($postObj->Eventkey == 'about_me') {
                         $this->weixinObj->responseText($postObj, '我是果果的爸爸');
+                    } else if ($postObj->Eventkey == 'dVvkdk'){
+                        $this->weixinObj->responseText($postObj, '谢谢你的称赞!');
                     } else {
                         $this->weixinObj->responseText($postObj, '其他东东');
                     }

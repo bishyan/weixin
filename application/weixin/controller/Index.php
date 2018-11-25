08f756580c24912c19d747b38578b56a1c73d363
bishyan <maijie5201314@163.com>
2018-11-25 14:02:14 +0800

    Update Index.php

--- application/weixin/controller/Index.php
+++ application/weixin/controller/Index.php
@@ -351,7 +351,7 @@ class Index extends Controller  {
     // 获取用户的open_id(不需要用户授权)(抽奖页面入口)
     public function getBaseInfo() {
         //dump($_GET);
-        dump($_SESSION);
+        dump(session());
         $res = $GLOBALS['HTTP_RAW_POST_DATA'];
         dump($res); exit;
         //抽奖活动的真正页面

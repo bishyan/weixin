1b5613dc4acb14b009b036a6f8af873b0742558a
bishyan <maijie5201314@163.com>
2018-11-26 23:41:47 +0800

    d

--- application/weixin/config.php
+++ application/weixin/config.php
@@ -3,7 +3,10 @@
 return [
     
     // 微信配置
-    'app_id' => 'wxf90f6aec3e2fcd91',
-    'secret' => '1830b09c31cdf066fa299025c326b8f3',
+    //'app_id' => 'wxf90f6aec3e2fcd91',
+    //'secret' => '1830b09c31cdf066fa299025c326b8f3',
+    
+    'app_id' => 'wxe6dfc606143872e8',
+    'secret' => 'Obe5d18ee3dOf3df973dO6c2a9e22b22',
     
 ];
